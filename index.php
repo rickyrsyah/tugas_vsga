20f7d6d3ccd6cfd7cdd4944623126ecbd59e6d49
Ricky R. Syahputra <rikysyahputra21@hotmail.com>
2019-07-16 14:51:14 +0700

    Initial Project

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Latihan Pertemuan ke-11</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<script src="assets/js/jquery.slim.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">DIGITAL TALENT</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Entri Data
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="view/tambah-produk.php">Tambah Produk</a>
+            <a class="dropdown-item" href="view/tambah-kategori.php">Tambah Kategori</a>
+            <a class="dropdown-item" href="view/tambah-merk.php">Tambah Merk</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="view/lihat-data.php">View Data</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+	<div class="container">
+		<h2 class="text-center">Pelatihan VSGA dengan tema Junior Web Developer hari ke-11</h2>
+	</div>
+</body>
+</html>
